9e8e12b460de457ce128c1f018f0d942fa32d45d
ssbigslatt <mapamulachristian@gmail.com>
2026-06-02 07:38:24 +1000

    Fix: Ensure SQLite3 config and clean dependencies

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -59,15 +59,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'rapidsniper_db',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
